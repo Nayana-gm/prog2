e7aef4c5f7de4cf46f999d5bb084d3de443d0484
Nayana-gm <nainagm2004@gmail.com>
2025-03-24 15:01:57 +0530

    Add.java

--- Add.java
+++ Add.java
@@ -1,8 +1,8 @@
 public class Add{
   public static void main(String[] args){
-    int x=5;
-    int y=2;
-    int z=x+y;
-    System.out.println("The sum is:"+c);
+    int a=5;
+    int b=2;
+    int c=a+b;
+    System.out.println("The sum is:"+);
   }
 }
